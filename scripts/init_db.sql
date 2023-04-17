b9a8450fee2e1bf977951f4fd210db3974e2800b
Chompakorn CChaichot <chompakornc_pro@vistec.ac.th>
2023-04-17 13:37:01 +0700

    fix db initialization command 🧳

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -2,15 +2,6 @@ CREATE DATABASE connext;
 
 USE connext;
 
-CREATE TABLE Token (
-  name VARCHAR(255) NOT NULL,
-  symbol VARCHAR(255) NOT NULL,
-  address VARCHAR(255) NOT NULL,
-  `decimal` INT NOT NULL,
-  chain INT NOT NULL,
-  PRIMARY KEY (address)
-);
-
 CREATE TABLE PolygonTransactions (
   id INT NOT NULL AUTO_INCREMENT,
   timestamp INT NOT NULL,
@@ -22,7 +13,7 @@ CREATE TABLE PolygonTransactions (
   action VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
+CREATE INDEX user_token_index ON PolygonTransactions (user_address, token_address, chain);
 CREATE TABLE ArbitrumTransactions (
   id INT NOT NULL AUTO_INCREMENT,
   timestamp INT NOT NULL,
@@ -34,7 +25,7 @@ CREATE TABLE ArbitrumTransactions (
   action VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
+CREATE INDEX user_token_index ON ArbitrumTransactions (user_address, token_address, chain);
 CREATE TABLE OptimismTransactions (
   id INT NOT NULL AUTO_INCREMENT,
   timestamp INT NOT NULL,
@@ -46,7 +37,7 @@ CREATE TABLE OptimismTransactions (
   action VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
+CREATE INDEX user_token_index ON OptimismTransactions (user_address, token_address, chain);
 CREATE TABLE BNBChainTransactions (
   id INT NOT NULL AUTO_INCREMENT,
   timestamp INT NOT NULL,
@@ -58,7 +49,7 @@ CREATE TABLE BNBChainTransactions (
   action VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
+CREATE INDEX user_token_index ON BNBChainTransactions (user_address, token_address, chain);
 CREATE TABLE GnosisTransactions (
   id INT NOT NULL AUTO_INCREMENT,
   timestamp INT NOT NULL,
@@ -70,10 +61,14 @@ CREATE TABLE GnosisTransactions (
   action VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
-
-CREATE INDEX address_chain_index ON Token (address, chain);
-CREATE INDEX user_token_index ON PolygonTransactions (user_address, token_address, chain);
-CREATE INDEX user_token_index ON ArbitrumTransactions (user_address, token_address, chain);
-CREATE INDEX user_token_index ON OptimismTransactions (user_address, token_address, chain);
-CREATE INDEX user_token_index ON BNBChainTransactions (user_address, token_address, chain);
 CREATE INDEX user_token_index ON GnosisTransactions (user_address, token_address, chain);
+
+CREATE TABLE Token (
+  name VARCHAR(255) NOT NULL,
+  symbol VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  `decimal` INT NOT NULL,
+  chain INT NOT NULL,
+  PRIMARY KEY (address)
+);
+CREATE INDEX address_chain_index ON Token (address, chain);
